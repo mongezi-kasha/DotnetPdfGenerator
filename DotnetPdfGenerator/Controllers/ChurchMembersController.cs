5a0e0829d6e8869202817e35fc64f352a40857bf
Mongezi Kasha <mongezikasha@gmail.com>
2024-10-28 17:11:32 +0200

    Added logo and removed hardcoded values, getting item thiur a request

---
+++ DotnetPdfGenerator/Controllers/ChurchMembersController.cs
@@ -0,0 +1,38 @@
+﻿using DotnetPdfGenerator.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PuppeteerSharp;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+namespace DotnetPdfGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChurchMembersController : ControllerBase
+    {
+        [HttpPost("get-report")]
+
+        public async Task<IActionResult> GetChurchReport(IEnumerable<ChurchMembers> churchMembers)
+        {
+            var options = new LaunchOptions
+            {
+                Headless = true,
+            };
+
+            var browser = new BrowserFetcher();
+            await browser.DownloadAsync();
+
+            using var browse = await Puppeteer.LaunchAsync(options);
+            using var page = await browse.NewPageAsync();
+            using var memoryStream = new MemoryStream();
+
+            var url = Url.ActionLink("Index", "PdfGenerator", new {model = JsonSerializer.Serialize(churchMembers)});
+            await page.GoToAsync(url);
+
+            var pdfstream = await page.PdfDataAsync();
+
+            return File(pdfstream, "application/pdf", "churchMembers.pdf");
+        }
+    }
+}
--- DotnetPdfGenerator/Controllers/PdfGenerator.cs
+++ DotnetPdfGenerator/Controllers/PdfGenerator.cs
@@ -1,55 +1,14 @@
 ﻿using DotnetPdfGenerator.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 
 namespace DotnetPdfGenerator.Controllers
 {
     public class PdfGenerator : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string model)
         {
-            var Ypddepartment = new ChurchDepartments
-            {
-                Name = "YPD"
-            };
-
-            var umanyamoDepartment = new ChurchDepartments
-            {
-                Name = "Umanyano lomama"
-            };
-            var youthChoir = new ChurchDepartments
-            {
-                Name = "Choir"
-            };
-
-            var churchMembers = new List<ChurchMembers>
-            {
-                new ChurchMembers
-                {
-                    MemberId = 1,
-                    FirstName = "Sive",
-                    LastName = "Manyube",
-                    Role = "Drummer",
-                    Departments = Ypddepartment
-                },
-
-                new ChurchMembers
-                {
-                    MemberId = 2,
-                    FirstName = "Mama u",
-                    LastName = "Bheja",
-                    Role = "Prayer",
-                    Departments = umanyamoDepartment
-                },
-
-                new ChurchMembers
-                {
-                    MemberId = 3,
-                    FirstName = "Khanya",
-                    LastName = "Nkumanda",
-                    Role = "Taking Videos",
-                    Departments = youthChoir
-                }
-            };
+            var churchMembers = JsonSerializer.Deserialize<IEnumerable<ChurchMembers>>(model);
             return View(churchMembers);
         }
     }
